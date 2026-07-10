6abde4c6941cab5fa37915f7da1fac5099743209
tesla <gablslcontact@gmail.com>
2026-07-09 21:48:55 -0300

    fix: wrong multiple migrations

--- api/src/main/resources/db/migration/V1__create_table_sources.sql
+++ api/src/main/resources/db/migration/V1__create_table_sources.sql
@@ -1,9 +1,10 @@
 CREATE TABLE sources (
-    id UUID DEFAULT gen_random_uuid() primary key,
+    id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
     name varchar(255) NOT NULL,
+    url VARCHAR(512) NOT NULL,
     language VARCHAR(50) NOT NULL,
-    status VARCHAR(50) NOT NULL,
     description TEXT,
+    active BOOLEAN NOT NULL DEFAULT TRUE,
     created_at TIMESTAMP WITHOUT TIME ZONE DEFAULT NOW(),
     updated_at TIMESTAMP WITHOUT TIME ZONE DEFAULT NOW()
 )
---
+++ api/src/main/resources/db/migration/V2__create_table_article.sql
@@ -0,0 +1,11 @@
+CREATE TABLE article (
+     id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+     title VARCHAR(255) NOT NULL,
+     description TEXT,
+     url VARCHAR(512) NOT NULL UNIQUE,
+     published_at TIMESTAMPTZ NOT NULL,
+     created_at TIMESTAMPTZ DEFAULT NOW(),
+     language VARCHAR(20) NOT NULL,
+     source_id UUID NOT NULL,
+     CONSTRAINT fk_article_source FOREIGN KEY (source_id) REFERENCES sources (id)
+);

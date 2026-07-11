b621a8b31664eaa05ce5a1333c8bef1fe0c769f5
tesla <gablslcontact@gmail.com>
2026-07-11 01:26:05 -0300

    feat: add cron to clean up old articles

--- api/src/main/java/com/safenews/api/service/ArticleService.java
+++ api/src/main/java/com/safenews/api/service/ArticleService.java
@@ -9,16 +9,20 @@ import com.safenews.api.model.Source;
 import com.safenews.api.repository.ArticleRepository;
 import com.safenews.api.repository.SourceRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.OffsetDateTime;
 import java.util.Optional;
 import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class ArticleService {
     private final ArticleRepository articleRepository;
     private final SourceRepository sourceRepository;
@@ -65,5 +69,16 @@ public class ArticleService {
         }
 
         articleRepository.deleteById(id);
+    }
+
+    @Scheduled(cron = "0 0 3 * * ?")
+    public void cleanupOldArticles() {
+        log.info("Starting the auto clean of old articles");
+
+        OffsetDateTime retentionLimit = OffsetDateTime.now().minusDays(7);
+
+        articleRepository.deleteArticleOlderthan(retentionLimit);
+
+        log.info("Cleaning completed successfully");
     }
 }
